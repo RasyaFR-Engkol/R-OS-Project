af713f499ac49df8992878efdc63c2259e662d8e
ConoCS <conoindocsgame@gmail.com>
2025-12-03 11:09:34 +0700

    Task.hpp update

--- Include/task.hpp
+++ Include/task.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
 #include "../kernel/task/task.hpp"
+
+// 
+// =====================================================
+// 
+//           RESERVED TASK INCLUDE STRUCTURES
+// 
+// =====================================================
+
+//
+// Input Daemon Task Includes
+#include "../kernel/task/reserved/inputdaemon/inputd.hpp"
+#include "../kernel/task/reserved/inputdaemon/inputm.hpp"
