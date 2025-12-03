539592c1eeefcfa269a47226c023ac172db5e1b7
ConoCS <conoindocsgame@gmail.com>
2025-12-03 11:13:16 +0700

    Update:
    Added a fully working internet stack including TCP, UDP, and ICMP, with multi-socket support to allow more simultaneous connections.
    
    Added a new EthernetStack layer for sending packets to the NIC (currently still hardcoded for Intel E1000 — ideas for generalization will be explored).
    
    Added DHCP Discovery so your device can obtain an IP address and identify itself on the network.

---
+++ Include/network/socket_fs.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <network/ethernet.hpp>
+#include <filesystem/filesystem.hpp>
+#include <network/tcp.hpp>
+#include <network/udp.hpp>
+#include <../kernel/driver/e1000/e1000.hpp>
+
+struct SocketFile : public File{
+    enum SockType { TCP, UDP, ICMP };
+    SockType Type;
+
+    union{
+        Network::TCPSocket* Tcp;
+        Network::UDPSocket* Udp;
+        Network::ICMPSocket *Icmp;
+    } Handle ;
+
+    SocketFile() {
+        RefCount = 1;
+        IsDirectory = FALSE;
+        FileSize = 0;
+        CurrentPosition = 0;
+    }
+
+};
+
+class SocketFileSystem : public FileSystem{
+    public:
+        static SocketFileSystem *Instance;
+        static SocketFileSystem *GetInstance();
+        U32 Read(File* file, U8* Buffer, U32 Size) override;
+
+        U32 Write(File* file, U8* Buffer, U32 Size) override;
+
+        void Close(File* file) override;
+
+        BOOL Mount(Partition*) override { return TRUE; }
+        File* Open(const char*) override { return nullptr; }
+        File* Create(const char*) override { return nullptr; }
+        BOOL Delete(const char*) override { return FALSE; }
+        BOOL Rename(const char*, const char*) override { return FALSE; }
+        BOOL Seek(File*, U64) override { return FALSE; }
+        INTN Ioctl(File*, U32, U64) override { return 0; }
+        BOOL Truncate(File*, U64) override { return FALSE; }
+        BOOL MKDir(const char*) override { return FALSE; }
+        BOOL RMDir(const char*) override { return FALSE; }
+        BOOL Flush(File*) override { return TRUE; }
+        BOOL Append(File*, U8*, U32) override { return FALSE; }
+        BOOL Cp(const char*, const char*) override { return FALSE; }
+        INTN ReadDir(File*, void*, U32) override { return -1; }
+};
