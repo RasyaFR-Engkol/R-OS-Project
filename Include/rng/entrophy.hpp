41f0f81ab3fabd2faa8e997a4ce4af85e29bb55d
RasyaFR-Engkol <conoindocsgame@gmail.com>
2026-01-05 18:25:41 +0700

    Entrophy: Adding random system

--- Include/rng/entrophy.hpp
+++ Include/rng/entrophy.hpp
@@ -14,4 +14,14 @@ class EntrophySystem{
         static U32 GetSeed(){
             return m_Pool;
         }
+
+        // Simple Xorshift RNG based on entropy pool
+        static U32 Random() {
+            U32 x = m_Pool;
+            x ^= x << 13;
+            x ^= x >> 17;
+            x ^= x << 5;
+            m_Pool = x; // Update pool biar next random beda
+            return x;
+        }
 };
